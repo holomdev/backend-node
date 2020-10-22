b07049b1c47621057a2d7fe571d606ed3938edc1
holomdev <holom.dev@gmail.com>
2020-10-21 22:37:20 -0500

    Sistema operativo y sistema de archivos

---
+++ so-archivos/async-files.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const file = process.argv[2];
+if(!file) {
+    throw new Error('No indicaste el archivo a leer');
+}
+
+const content = fs.readFile(file, function (err,content) {
+    if(err) {
+        return console.log(err);
+    }
+    const lines = content.toString().split('\n').length;
+    console.log(lines);
+});
+
---
+++ so-archivos/os.js
@@ -0,0 +1,8 @@
+const os = require('os');
+
+// console.log("CPU info: ", os.cpus());
+// console.log("IP address: ", os.networkInterfaces());
+// console.log("Free memory: ", os.freemem() );
+// console.log("OS Type: ", os.type());
+// console.log("OS version: ", os.release());
+console.log("User info: ", os.userInfo());
